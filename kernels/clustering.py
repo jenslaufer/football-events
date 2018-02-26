7b4ab7c6b04fd873cff5e71ba4668492a6587a6f
Jens Laufer <jenslaufer@gmail.com>
2018-02-26 16:22:46 +0100

    changed

--- kernels/clustering.py
+++ kernels/clustering.py
@@ -1,7 +1,18 @@
 import numpy as np
 import pandas as pd
-import csv
+
+print('loading data sets...')
+
+events = pd.read_csv('../data/events.csv')
+
+events = events.merge(pd.read_csv('../data/ginf.csv'), how='inner')
+
+print('loading data sets succeeded.')
+
+events_cont = pd.get_dummies(events)
+
+print(events.head())
 
 
-events = csv.read('../data/events.csv')
+
 
